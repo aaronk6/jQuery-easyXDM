851eba0009812dc7a84463e0df5307a65df5470f
Kjell-Magne Øierud <kjellm@oierud.net>
2012-03-30 13:28:23 +0200

    untabify

--- jquery.easyXDM.js
+++ jquery.easyXDM.js
@@ -1,20 +1,16 @@
 (function($) {
-
-	  jQuery.ajaxTransport(function(options, originalOptions, jqXHR) {
-
-		    if(!$.support.cors) {
- 
+    jQuery.ajaxTransport(function(options, originalOptions, jqXHR) {
+        if(!$.support.cors) {
             return {
-               
                 send: function( headers, completeCallback ) {
                     if (!options.crossDomain) return;
-			              $.getScript("easy_xdm/easyXDM.min.js", function() {
+                    $.getScript("easy_xdm/easyXDM.min.js", function() {
                         if (!(typeof(window["JSON"]) == 'object' && window["JSON"])) {
-				                    $.getScript("easy_xdm/json2.js");
+                            $.getScript("easy_xdm/json2.js");
                         }
-				                var xhr = new easyXDM.Rpc({ remote: "easy_xdm/cors/index.html" },
+                        var xhr = new easyXDM.Rpc({ remote: "easy_xdm/cors/index.html" },
                                                   { remote: { request: {} } });
-				                var success = function(response){
+                        var success = function(response){
                             var status = response.status;
                             var statusText = response.data;
                             var responses;
@@ -23,8 +19,8 @@
                             }
                             var headers = response.headers;
                             completeCallback(status, statusText, responses, headers);
-				                };
-				                var error = function(e){
+                        };
+                        var error = function(e){
                             var status = e.status;
                             var statusText = e.data;
                             var responses;
@@ -33,19 +29,16 @@
                             }
                             var headers = null;
                             completeCallback(status, statusText, responses, headers);
-				                };
-				                xhr.request({
-				                    url      : options.url,
-				                    method   : options.type,
-				                    data     : options.data,
-				                }, success, error);
-			              });
+                        };
+                        xhr.request({
+                            url      : options.url,
+                            method   : options.type,
+                            data     : options.data,
+                        }, success, error);
+                    });
                 },
-
                 abort: function() {}
             };
-
         }
     });
-
 })(jQuery);
