cd4e7fd8b56e5711ddab68b6fcfdb181e8051caa
Jorgen H. Fjeld <jorgen@veridit.no>
2012-05-09 11:41:49 +0200

    Rewritten jquery.easyXDM plugin to: * Only use a single easyXDM instance, to avoid conflicts. * Use our own easyXDM provider. ** This fixes the bug where the default easyXDM cors provider would    append an extra "?" to each request. ** This makes our plugin support anything that $.ajax supports,    and solves all the known problems.

--- jquery.easyXDM.js
+++ jquery.easyXDM.js
@@ -1,44 +1,84 @@
+var jquery_easyXDM = {};
 (function($) {
-    jQuery.ajaxTransport(function(options, originalOptions, jqXHR) {
-        if(!$.support.cors) {
-            return {
-                send: function( headers, completeCallback ) {
-                    if (!options.crossDomain) return;
-                    $.getScript("easy_xdm/easyXDM.min.js", function() {
-                        if (!(typeof(window["JSON"]) == 'object' && window["JSON"])) {
-                            $.getScript("easy_xdm/json2.js");
-                        }
-                        var xhr = new easyXDM.Rpc({ remote: "easy_xdm/cors/index.html" },
-                                                  { remote: { request: {} } });
-                        var success = function(response){
-                            var status = response.status;
-                            var statusText = response.data;
-                            var responses;
-                            if (options.dataType === "json") {
-                                responses = { "json": $.parseJSON(statusText) };
-                            }
-                            var headers = response.headers;
-                            completeCallback(status, statusText, responses, headers);
-                        };
-                        var error = function(e){
-                            var status = e.status;
-                            var statusText = e.data;
-                            var responses;
-                            if (options.dataType === "json") {
-                                responses = { "json": $.parseJSON(statusText) };
-                            }
-                            var headers = null;
-                            completeCallback(status, statusText, responses, headers);
-                        };
-                        xhr.request({
-                            url      : options.url,
-                            method   : options.type,
-                            data     : options.data,
-                        }, success, error);
-                    });
-                },
-                abort: function() {}
-            };
-        }
-    });
+  // Used pattern from http://backtothefront.net/2012/asynchronous-singleton-api/
+  // for thread safe single instance easyXDM instatiation.
+  var state = "before";
+  var callbackQueue = [];
+  var easyXDM_connection = null;
+  var error = null;
+  var doRequests = function(callbacks) {
+      if(!$.support.cors) {
+        // Allow debugging of easyXDM by adding easyXDM_debug=true as parameter to the page that uses this plugin.
+        var easyXDM_debug = false;
+        if(/jquery\.easyXDM\.debug=true/.test(window.location)){ easyXDM_debug = true; };
+        var easyXDM_url = "/easyXDM/easyXDM.min.js";
+        if(easyXDM_debug) { easyXDM_url = "/easyXDM/easyXDM.debug.js" };
+        // TODO: Replace with jQuery.cachedScript from http://api.jquery.com/jQuery.getScript/
+        $.getScript(easyXDM_url, function() {
+          //var scoped_easyXDM = easyXDM.noConflict("jquery_easyXDM");
+          //window.jquery_easyXDM.easyXDM = scoped_easyXDM;
+          var remote_url = "/javascripts/jquery.easyXDM.provider.html";
+          if(easyXDM_debug) { remote_url += "?jquery.easyXDM.debug=true" };
+          easyXDM_connection = new easyXDM.Rpc({ remote: remote_url }, { remote: { jquery_proxy: {} } });
+          callbacks.success(easyXDM_connection);
+        });
+      };
+    };
+
+  jquery_easyXDM.getConnection = function(callbacks) {
+    switch (state) {
+      case "before":
+        state = "working";
+        callbackQueue.push(callbacks);
+        doRequests({
+          success : function(singletonInstance) {
+            state = "success";
+            easyXDM_connection = singletonInstance;
+            for (var i = 0, ilen = callbackQueue.length; i < ilen; i++) {
+              callbackQueue[i].success(easyXDM_connection);
+            }
+            callbackQueue = [];
+          },
+          failure : function(errorObj) {
+            state = "failure";
+            error = errorObj;
+            for (var i = 0, ilen = callbackQueue.length; i < ilen; i++) {
+              callbackQueue[i].failure(error);
+            }
+            callbackQueue = [];
+          }
+        });
+        break;
+      case "working":
+        callbackQueue.push(callbacks);
+        break;
+      case "success":
+        callbacks.success(easyXDM_connection);
+        break;
+      case "failure":
+        callbacks.failure(error);
+        break;
+      default:
+        throw new Error("Invalid state: " + state)
+    }
+  };
+
+  jQuery.ajaxTransport(function(options, originalOptions, jqXHR) {
+    if(!$.support.cors && options.crossDomain) {
+      return {
+        send: function( headers, completeCallback ) {
+          jquery_easyXDM.getConnection({
+            success: function(easyXDM_connection){
+              function continuation_proxy(results){
+                completeCallback(results.status, results.statusText, results.responses, results.headers);
+              };
+              easyXDM_connection.jquery_proxy(options,continuation_proxy);
+            },
+            failure : function(){ }
+          });
+        },
+          abort: function() {}
+      };
+    }
+  });
 })(jQuery);
