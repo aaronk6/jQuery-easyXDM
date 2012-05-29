3b2f7796b468400a838c4bf617f914772900e778
Tatjana Andersen <tatjana.andersen@redpill-linpro.com>
2012-05-29 23:15:42 +0200

    Indentation update.

--- jquery.easyXDM.provider.js
+++ jquery.easyXDM.provider.js
@@ -1,41 +1,60 @@
-/* 
- * This is a CORS (Cross-Origin Resource Sharing) and AJAX enabled endpoint for jquery.easyXDM.
+/*
+ * https://github.com/GyldendalDigital/jQuery-EasyXDM
+ *
+ * This is a CORS (Cross-Origin Resource Sharing) and AJAX enabled endpoint
+ * for jQuery-EasyXDM plugin.
  * It proxys jquery.easyXDM requests with an easyXDM provider.
+ *
  */
-(function($) {
+(function ($) {
   // Load correct version of easyXDM
   var easyXDM_debug = false;
-  if(/jquery\.easyXDM\.debug=true/.test(window.location)){ easyXDM_debug = true; };
+  if (/jquery\.easyXDM\.debug=true/.test(window.location)) {
+    easyXDM_debug = true;
+  };
   var easyXDM_url = "/easyXDM/easyXDM.min.js";
-  if(easyXDM_debug) { easyXDM_url = "/easyXDM/easyXDM.debug.js" };
-  $.getScript(easyXDM_url).done(function(){
+  if (easyXDM_debug) {
+    easyXDM_url = "/easyXDM/easyXDM.debug.js"
+  };
+  $.getScript(easyXDM_url).done(function () {
     // Use the scoped easyXDM available as a unique global name in the "parent",
     // and must match the noConflict name in parent.
     var scoped_easyXDM = easyXDM.noConflict("jquery_easyXDM");
-    // instantiate a new easyXDM object which will handle the request 
-    var remote = new scoped_easyXDM.Rpc({ local: "name.html", swf: "easyxdm.swf"}, {
-      local: {
+    // instantiate a new easyXDM object which will handle the request
+    var remote = new scoped_easyXDM.Rpc({ local:"name.html", swf:"easyxdm.swf"}, {
+      local:{
         // define the exposed method
-        jquery_proxy: function(config, continuation_proxy){
-          $.ajax(config).done(function(data,textStatus,jqXHR){
-            var result = {status: jqXHR.status,
-              statusText: jqXHR.statusText,
-              responses: {},
-              headers: jqXHR.getAllResponseHeaders()
-            } ;
-            if(jqXHR.responseText){ result.responses.text = jqXHR.responseText }
-            if(jqXHR.responseXml) { result.responses.xml  = jqXHR.responseXml }
-            continuation_proxy(result);
-          }).fail(function(jqXHR,textStatus,errorMessage){
-            var result = {status: jqXHR.status,
-              statusText: jqXHR.statusText,
-              responses: {},
-              headers: jqXHR.getAllResponseHeaders()
-            } ;
-            if(jqXHR.responseText){ result.responses.text = jqXHR.responseText }
-            if(jqXHR.responseXml) { result.responses.xml  = jqXHR.responseXml }
-            continuation_proxy(result);
-          });
+        jquery_proxy:function (config, continuation_proxy) {
+          $.ajax(config).done(
+            function (data, textStatus, jqXHR) {
+              var result = {
+                status    :jqXHR.status,
+                statusText:jqXHR.statusText,
+                responses :{},
+                headers   :jqXHR.getAllResponseHeaders()
+              };
+              if (jqXHR.responseText) {
+                result.responses.text = jqXHR.responseText
+              }
+              if (jqXHR.responseXml) {
+                result.responses.xml = jqXHR.responseXml
+              }
+              continuation_proxy(result);
+            }).fail(function (jqXHR, textStatus, errorMessage) {
+              var result = {
+                status    :jqXHR.status,
+                statusText:jqXHR.statusText,
+                responses :{},
+                headers   :jqXHR.getAllResponseHeaders()
+              };
+              if (jqXHR.responseText) {
+                result.responses.text = jqXHR.responseText
+              }
+              if (jqXHR.responseXml) {
+                result.responses.xml = jqXHR.responseXml
+              }
+              continuation_proxy(result);
+            });
         }
       }
     });
