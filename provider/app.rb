eec44a8f20a1961c59425b04d29568bb126e96eb
Kent Hansen <kent.hansen@gyldendal.no>
2012-11-14 14:05:07 +0100

    Add tests for checking cookie transmission when withCredentials=true
    Had to add the Cache-Control header on the server to keep IE from
    caching the xhrs GET request responses (which caused the cookie to have
    the wrong value).

--- provider/app.rb
+++ provider/app.rb
@@ -8,7 +8,8 @@ end
 
 before do
   headers('Access-Control-Allow-Origin' => request.env['HTTP_ORIGIN'] || '*',
-          'Access-Control-Allow-Credentials' => 'true')
+          'Access-Control-Allow-Credentials' => 'true',
+          'Cache-Control' => 'max-age=0, private')
 end
 
 options '/*' do
@@ -27,4 +28,32 @@ end
 post '/test_post.js' do
   content_type :json
   {"message" => "POST with successful server response", "foo" => params[:foo]}.to_json
+end
+
+get '/test_get_with_cookie_return.js' do
+  headers('Set-Cookie' => 'jquery_easyXDM_test_cookie=level1; Path=/; Max-Age=100')
+  content_type :json
+  {"message" => "GET with successful server response", "foo" => params[:foo]}.to_json
+end
+
+get '/test_get_with_credentials.js' do
+  content_type :json
+  if request.cookies['jquery_easyXDM_test_cookie'] == 'level1'
+    headers('Set-Cookie' => 'jquery_easyXDM_test_cookie=level2; Path=/; Max-Age=100')
+    {"message" => "GET with successful server response", "foo" => params[:foo]}.to_json
+  else
+    puts "OOPS! Wrong cookie value(s) received: #{request.cookies.inspect}"
+    {"message" => "GET with unsuccessful server response", "cookies" => request.cookies}.to_json
+  end
+end
+
+post '/test_post_with_credentials.js' do
+  content_type :json
+  if request.cookies['jquery_easyXDM_test_cookie'] == 'level2'
+    headers('Set-Cookie' => 'jquery_easyXDM_test_cookie=level3; Path=/; Max-Age=100')
+    {"message" => "POST with successful server response", "foo" => params[:foo]}.to_json
+  else
+    puts "OOPS! Wrong cookie value(s) received: #{request.cookies.inspect}"
+    {"message" => "GET with unsuccessful server response", "cookies" => request.cookies}.to_json
+  end
 end
